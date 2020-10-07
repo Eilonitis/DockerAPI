5e30a81c57865a63c07272ab840a3b6fc0a32b5d
Eilon Weiner <3eilon@gmail.com>
2020-10-07 03:54:37 +0300

    init

---
+++ Controllers/BlaController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerAPI.Controllers
+{
+    [ApiController]
+    public class BlaController : ControllerBase
+    {
+        public BlaController()
+        {
+            
+        }
+
+        [HttpGet]
+        [Route("[controller]/[action]")]
+        public string Hello()
+        {
+            string returnStr = "hello there";
+            return returnStr;
+        }
+    }
+}
